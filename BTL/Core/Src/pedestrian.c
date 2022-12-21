605d5ea23fef6618b578b804fd4de889107b6293
ThePhong1803 <112880913+ThePhong1803@users.noreply.github.com>
2022-12-21 21:26:44 +0700

    minor fixes

--- BTL/Core/Src/pedestrian.c
+++ BTL/Core/Src/pedestrian.c
@@ -10,11 +10,16 @@
 void fsm_pedestrian() {
 	switch (status4) {
 	case PEDESTRIAN_RUN_ALLOW:
+		// Turn on led which allow pedestrian go
 		HAL_GPIO_WritePin(L2_EN0_GPIO_Port, L2_EN0_Pin, RESET);
 		HAL_GPIO_WritePin(L2_EN1_GPIO_Port, L2_EN1_Pin, SET);
+
+		// Change mode when timer expired
 		if (isTimer5Expired()) {
 			status4 = PEDESTRIAN_OFF;
 		}
+
+		// Setup buzzer
 		if (isTimer6Expired()) {
 			if (timeCountdown1 <= 5) {
 				__HAL_TIM_SetCompare(&htim3,TIM_CHANNEL_1,(5-timeCountdown1)*20);
@@ -22,33 +27,47 @@ void fsm_pedestrian() {
 			}
 		}
 
+		// Change mode when auto mode change
 		if (status1 == AUTO_GREEN || status1 == AUTO_YELLOW) {
 			status4 = PEDESTRIAN_RUN_NOT_ALLOW;
 			__HAL_TIM_SetCompare(&htim3,TIM_CHANNEL_1,0);
 		}
+
+		// Refresh time if user request
 		if (isButtonPressed(3)) {
 			setTimer5(1000);
 		}
 		break;
+
 	case PEDESTRIAN_RUN_NOT_ALLOW:
+		// Turn on led which not allow pedestrian go
 		HAL_GPIO_WritePin(L2_EN0_GPIO_Port, L2_EN0_Pin, SET);
 		HAL_GPIO_WritePin(L2_EN1_GPIO_Port, L2_EN1_Pin, RESET);
+
+		// Change mode when timer expired
 		if (isTimer5Expired()) {
 			status4 = PEDESTRIAN_OFF;
 			__HAL_TIM_SetCompare(&htim3,TIM_CHANNEL_1,0);
 		}
+
+		// Change mode when auto mode change
 		if (status1 == AUTO_RED) {
 			status4 = PEDESTRIAN_RUN_ALLOW;
 			setTimer6(100);
 		}
+
+		// Refresh time when user request
 		if (isButtonPressed(3)) {
 			setTimer5(1000);
 		}
 		break;
+
 	case PEDESTRIAN_OFF:
+		// Turn off led and buzzer
 		HAL_GPIO_WritePin(L2_EN0_GPIO_Port, L2_EN0_Pin, RESET);
 		HAL_GPIO_WritePin(L2_EN1_GPIO_Port, L2_EN1_Pin, RESET);
 		__HAL_TIM_SetCompare(&htim3,TIM_CHANNEL_1,0);
+		// Change mode when user request corresponding with auto mode
 		if (status1 != WAIT) {
 			if(status1 == AUTO_RED && isButtonPressed(3)){
 				status4 = PEDESTRIAN_RUN_ALLOW;
@@ -62,6 +81,7 @@ void fsm_pedestrian() {
 		}
 		break;
 	default:
+		// Do nothing
 		break;
 	}
 }
